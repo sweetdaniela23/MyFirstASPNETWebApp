da63f5a99a220c3d62208c4df5ebed714ed788c5
sweetdaniela23 <90408575+sweetdaniela23@users.noreply.github.com>
2022-01-10 07:17:30 -0700

    Add project files.

---
+++ MyFirstASPNETWebApp/wwwroot/js/FanMail.js
@@ -0,0 +1,10 @@
+﻿document.getElementById("btnSend").addEventListener("click", function () {
+    alert("From: " + document.getElementById("txtFrom").value +
+        ", Subject: " + document.getElementById("txtSubject").value +
+        ", Message: " + document.getElementById("txtMessage").value);
+    $("#picGanderson").fadeToggle();
+})
+
+//document.getElementById("btnSend").addEventListener("click", function () {
+//    alert("This Worked!");
+//})
